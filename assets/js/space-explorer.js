a2f698c36157bceac9e1aced013e79097026347c
Paul Marren <paul.marren.coding@gmail.com>
2025-01-31 14:45:02 +0000

    Created canvas variables and set the dimesions of the canvas in space-explorer.js

---
+++ assets/js/space-explorer.js
@@ -0,0 +1,17 @@
+// Select the canvas wrapper
+const canvasWrapper = document.getElementById('canvas-wrapper');
+// Create canvas
+const canvas = document.createElement('canvas');
+// Create 2d context object
+const ctx = canvas.getContext('2d');
+
+// Set canvas dimensions
+const dpr = window.devicePixelRatio || 1;
+canvas.width = canvasWrapper.offsetWidth * dpr;
+canvas.height = canvasWrapper.offsetHeight * dpr;
+canvas.style.width = `${canvasWrapper.offsetWidth}px`;
+canvas.style.height = `${canvasWrapper.offsetHeight}px`;
+ctx.scale(dpr, dpr);
+
+// Add canvas to canvasWrapper
+canvasWrapper.appendChild(canvas);
